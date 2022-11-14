0f38b8b7bf47447240bb15d976dcf014b898b9cc
Guilherme Correia <guicorrea384@gmail.com>
2022-11-13 21:49:10 -0300

    :ambulance: Use original CDN for GoatCounter.js

--- enhanceAppFile.js
+++ enhanceAppFile.js
@@ -1,15 +1,11 @@
 
 export default ({ router }) => {
   if (process.env.NODE_ENV === 'production' && USER && typeof window !== 'undefined') {
-    (function() {
-       var goatcounter = document.createElement("script")
-	   goatcounter.setAttribute("data-goatcounter","https://"+USER+".goatcounter.com/count")
-	   goatcounter.setAttribute("async","true");
-//	   goatcounter.setAttribute("src","//gc.zgo.at/count.js");
-       goatcounter.setAttribute("src","https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master/assets/js/count.js");
-       var s = document.getElementsByTagName("script")[0]
-       s.parentNode.insertBefore(goatcounter, s)
-    })()
+    let goatcounter = document.createElement("script")
+    goatcounter.setAttribute("data-goatcounter", `https://${USER}.goatcounter.com/count`)
+    goatcounter.setAttribute("async", "true")
+    goatcounter.setAttribute("src","//gc.zgo.at/count.js")
+    let s = document.getElementsByTagName("script")[0]
+    s.parentNode.insertBefore(goatcounter, s)
   }
 }
-
